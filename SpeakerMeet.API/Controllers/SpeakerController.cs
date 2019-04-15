d2ad333b2dde2038471468326f3717e1451686f8
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-04-15 16:24:15 -0400

    Add project files.

---
+++ SpeakerMeet.API/Controllers/SpeakerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeakerMeet.API.Controllers
+{
+    public class Speaker
+    {
+        public string Name { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeakerController : ControllerBase
+    {
+        private readonly List<Speaker> _speakers;
+        public SpeakerController()
+        {
+            _speakers = new List<Speaker>() { 
+                new Speaker { 
+                    Name = "Jim Speaker"
+                    }, 
+                new Speaker { 
+                    Name = "Joshua Smith"
+                    },
+                new Speaker
+                {
+                    Name = "Josh Martin"
+                }
+                };
+        }
+
+        public IActionResult Search(string term)
+        {
+            return new OkObjectResult(_speakers.Where(s => s.Name.Contains(term)).ToList());
+        }
+    }
+}
---
+++ SpeakerMeet.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpeakerMeet.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SpeakerMeet.Tests
+{
+    public class SpeakerControllerSearchTests
+    {
+        private readonly SpeakerController _controller;
+
+        public SpeakerControllerSearchTests()
+        {
+            _controller = new SpeakerController();
+        }
+
+        [Fact]
+        public void ItReturnsOkObjectResult()
+        {
+            // Act
+            var result = _controller.Search("Jos");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void ItReturnsCollectionOfSpeakers()
+        {
+            // Act
+            var result = _controller.Search("Jos") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.IsType<List<Speaker>>(result.Value);
+        }
+
+        [Fact]
+        public void GivenExactMatchThenOneSpeakerInCollection()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search("Joshua") as OkObjectResult;
+
+            // Assert
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Single(speakers);
+        }
+    }
+}
